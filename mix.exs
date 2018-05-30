fe9d0aa2f6e7aceea6b1a3abe7911b8fa7fe1ae0
Kevin Brown <chevinbrown@gmail.com>
2018-05-30 06:32:49 -0500

    Add timex, httpoison, and remix

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule DuoClient.MixProject do
+defmodule Duo.MixProject do
   use Mix.Project
 
   def project do
@@ -14,15 +14,19 @@ defmodule DuoClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      applications: applications(Mix.env())
     ]
   end
+
+  defp applications(:dev), do: applications(:all) ++ [:remix]
+  defp applications(_all), do: [:logger, :httpoison, :timex]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.1.1"},
+      {:remix, "~> 0.0.1", only: :dev},
+      {:timex, "~> 3.1"}
     ]
   end
 end
