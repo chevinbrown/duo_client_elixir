07f7490996d70f4159e7f5a51b85ed253ad2da18
Kevin Brown <chevinbrown@gmail.com>
2018-05-30 06:41:21 -0500

    Add base case to check auth

--- lib/duo_client.ex
+++ lib/duo_client.ex
@@ -1,18 +1,46 @@
 defmodule DuoClient do
-  @moduledoc """
-  Documentation for DuoClient.
-  """
+  def check do
+    HTTPoison.get!("https://#{host()}#{path()}", headers())
+  end
 
-  @doc """
-  Hello world.
+  def headers do
+    [
+      {"Date", date_now()},
+      {"Authorization", "Basic #{build_authorization()}"},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+  end
 
-  ## Examples
+  def date_now do
+    {:ok, date} = Timex.format(Timex.now(), "%a, %d %b %Y %H:%M:%S -0000", :strftime)
+    date
+  end
 
-      iex> DuoClient.hello
-      :world
+  def build_authorization do
+    Base.encode64("#{ikey()}:#{auth_hash()}")
+  end
 
-  """
-  def hello do
-    :world
+  def auth_hash do
+    params = ""
+    request = date_now() <> "\n" <> "GET\n" <> host() <> "\n" <> path() <> "\n" <> params
+
+    :crypto.hmac(:sha, skey(), request)
+    |> Base.encode16()
+  end
+
+  defp path do
+    "/auth/v2/check"
+  end
+
+  defp ikey do
+    Application.get_env(:duo_client, :settings)[:ikey]
+  end
+
+  defp skey do
+    Application.get_env(:duo_client, :settings)[:skey]
+  end
+
+  defp host do
+    Application.get_env(:duo_client, :settings)[:host]
   end
 end
