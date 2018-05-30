3df72481a9f6e9fd8abfc8b76a344ebefa9f8058
Kevin Brown <chevinbrown@gmail.com>
2018-05-30 18:07:34 -0500

    Working example... Why waste time say lot word when few word do trick?

--- lib/duo_client.ex
+++ lib/duo_client.ex
@@ -1,12 +1,39 @@
 defmodule DuoClient do
+  @ikey Application.get_env(:duo_client, :settings)[:ikey]
+  @skey Application.get_env(:duo_client, :settings)[:skey]
+  @host Application.get_env(:duo_client, :settings)[:host]
+
   def check do
-    HTTPoison.get!("https://#{host()}#{path()}", headers())
+    path = "/auth/v2/check"
+    signature = build_authorization("POST", path, "")
+    HTTPoison.get!("https://#{@host}#{path}", headers(signature))
   end
 
-  def headers do
+  def preauth(identifier, ipaddr \\ "", trusted_device_token \\ "") do
+    path = "/auth/v2/preauth"
+    params = identifier
+    signature = build_authorization("POST", path, params)
+    resp = HTTPoison.post!("https://#{@host}#{path}", params, headers(signature))
+  end
+
+  def auth(identifier \\ "", factor \\ "") do
+    path = "/auth/v2/auth"
+    params = identifier
+    signature = build_authorization("POST", path, params)
+
+    HTTPoison.post!(
+      "https://#{@host}#{path}",
+      params,
+      headers(signature),
+      timeout: 50_000,
+      recv_timeout: 50_000
+    )
+  end
+
+  def headers(signature) do
     [
       {"Date", date_now()},
-      {"Authorization", "Basic #{build_authorization()}"},
+      {"Authorization", "Basic #{signature}"},
       {"Content-Type", "application/x-www-form-urlencoded"}
     ]
   end
@@ -16,31 +43,15 @@ defmodule DuoClient do
     date
   end
 
-  def build_authorization do
-    Base.encode64("#{ikey()}:#{auth_hash()}")
+  def build_authorization(method, path, params) do
+    "#{@ikey}:#{auth_hash(method, path, params)}"
+    |> Base.encode64()
   end
 
-  def auth_hash do
-    params = ""
-    request = date_now() <> "\n" <> "GET\n" <> host() <> "\n" <> path() <> "\n" <> params
+  def auth_hash(method, path, params) do
+    request = date_now() <> "\n" <> method <> "\n" <> @host <> "\n" <> path <> "\n" <> params
 
-    :crypto.hmac(:sha, skey(), request)
+    :crypto.hmac(:sha, @skey, request)
     |> Base.encode16()
-  end
-
-  defp path do
-    "/auth/v2/check"
-  end
-
-  defp ikey do
-    Application.get_env(:duo_client, :settings)[:ikey]
-  end
-
-  defp skey do
-    Application.get_env(:duo_client, :settings)[:skey]
-  end
-
-  defp host do
-    Application.get_env(:duo_client, :settings)[:host]
   end
 end
