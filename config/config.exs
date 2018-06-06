f5a15466199f89257d648813ad5d1d43a56a9192
Kevin Brown <chevinbrown@gmail.com>
2018-06-06 07:24:14 -0500

    Move remix to dev, rename secret.example to example for dev config

--- config/config.exs
+++ config/config.exs
@@ -28,8 +28,5 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-config :remix,
-  escript: true,
-  silent: true
 
 import_config "#{Mix.env()}.exs"
---
+++ config/dev.example.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :remix,
+  escript: true,
+  silent: true
+
+config :duo_client, :settings,
+  ikey: "",
+  skey: "",
+  host: ""
