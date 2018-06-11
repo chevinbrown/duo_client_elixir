4c8a9ad131a8117d3e7e23d08dcdf80c618aa450
Kevin Brown <chevinbrown@gmail.com>
2018-06-11 18:34:41 -0500

    Add ping and check tests, mocked. Not a probable pattern for continued testing...

--- test/duo_client_test.exs
+++ test/duo_client_test.exs
@@ -1,4 +1,30 @@
 defmodule DuoClientTest do
   use ExUnit.Case
   doctest DuoClient
+
+  import Tesla.Mock
+
+  setup do
+    mock(fn
+      %{method: :get, url: "https://api-secret.duosecurity.com/auth/v2/ping"} ->
+        %Tesla.Env{status: 200, body: %{"response" => %{"time" => 1_528_758_917}, "stat" => "OK"}}
+
+      %{method: :get, url: "https://api-secret.duosecurity.com/auth/v2/check"} ->
+        %Tesla.Env{status: 200, body: %{"response" => %{"time" => 1_528_758_917}, "stat" => "OK"}}
+    end)
+
+    :ok
+  end
+
+  test "ping" do
+    assert {:ok, %Tesla.Env{} = env} = DuoClient.get("/auth/v2/ping")
+    assert env.status == 200
+    assert env.body == %{"response" => %{"time" => 1_528_758_917}, "stat" => "OK"}
+  end
+
+  test "check" do
+    assert {:ok, %Tesla.Env{} = env} = DuoClient.get("/auth/v2/check")
+    assert env.status == 200
+    assert env.body == %{"response" => %{"time" => 1_528_758_917}, "stat" => "OK"}
+  end
 end
