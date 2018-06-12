0c581b8c542d5ffa2a3a6105e518c0bce1e911d3
Kevin Brown <chevinbrown@gmail.com>
2018-06-12 07:41:11 -0500

    Bump tesla version

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Duo.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:jason, ">= 1.0.0"},
       {:remix, "~> 0.0.1", only: :dev},
-      {:tesla, "1.0.0-beta.1"}
+      {:tesla, "1.0.0"}
     ]
   end
 end
